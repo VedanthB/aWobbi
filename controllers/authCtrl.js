23383c24c4459284689504f6b9ac6b1a6d918eb9
Vedanth Bora <vedanthbora@gmail.com>
2022-05-12 10:43:05 +0530

    [FEAT]: register api

---
+++ controllers/authCtrl.js
@@ -0,0 +1,95 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable consistent-return */
+/* eslint-disable camelcase */
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Users = require('../models/userModel');
+
+const createAccessToken = (payload) =>
+  jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '1d',
+  });
+
+const createRefreshToken = (payload) =>
+  jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: '30d',
+  });
+
+const authCtrl = {
+  register: async (req, res) => {
+    try {
+      const { fullName, email, password, userName, gender } = req.body;
+
+      const newUserName = userName.toLowerCase().replace(/ /g, '');
+
+      const user_name = await Users.findOne({ userName: newUserName });
+
+      if (user_name)
+        return res.status(400).json({ msg: 'This user name already exists' });
+
+      const user_email = await Users.findOne({ email });
+
+      if (user_email)
+        return res.status(400).json({ msg: 'This email already exists' });
+
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: 'The password must be 6 characters' });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = new Users({
+        fullName,
+        email,
+        password: passwordHash,
+        userName: newUserName,
+        gender,
+      });
+
+      const access_token = createAccessToken({ id: newUser._id });
+      const refresh_token = createRefreshToken({ id: newUser._id });
+
+      res.cookie('refreshtoken', refresh_token, {
+        httpOnly: true,
+        path: '/api/refresh_token',
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+      });
+
+      await newUser.save();
+
+      //   console.log(newUser);
+
+      res.json({
+        msg: 'Registered!',
+        access_token,
+        user: {
+          ...newUser._doc,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  //   login: async (req, res) => {
+  //     try {
+  //     } catch (error) {
+  //       return res.status(500).json({ msg: error.message });
+  //     }
+  //   },
+  //   logout: async (req, res) => {
+  //     try {
+  //     } catch (error) {
+  //       return res.status(500).json({ msg: error.message });
+  //     }
+  //   },
+  //   generateAccessToken: async (req, res) => {
+  //     try {
+  //     } catch (error) {
+  //       return res.status(500).json({ msg: error.message });
+  //     }
+  //   },
+};
+
+module.exports = authCtrl;
