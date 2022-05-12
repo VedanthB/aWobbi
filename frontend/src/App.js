491b45ddd9596483f762d0c44851d051962186f4
Vedanth Bora <vedanthbora@gmail.com>
2022-05-12 13:43:02 +0530

    [FEAT: frontend setup with tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl bg-gray-500 p-6 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
