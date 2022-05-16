f11a90711a1a039152882d96346141c73721ddb8
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-16 22:49:29 +0530

    [FEAT]: update user api

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 const Users = require('../models/userModel');
 
@@ -26,12 +27,41 @@ const userCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
-  //   updateUser: async (req, res) => {
-  //     try {
-  //     } catch (error) {
-  //       return res.status(500).json({ msg: error.message });
-  //     }
-  //   },
+  updateUser: async (req, res) => {
+    try {
+      const {
+        avatar,
+        userName,
+        fullName,
+        mobile,
+        address,
+        story,
+        website,
+        gender,
+      } = req.body;
+
+      if (!fullName)
+        return res.status(400).json({ msg: 'Please add your full name.' });
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          avatar,
+          userName,
+          fullName,
+          mobile,
+          address,
+          story,
+          website,
+          gender,
+        }
+      );
+
+      res.json({ msg: 'Updated profile details successfully.' });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
   //   follow: async (req, res) => {
   //     try {
   //     } catch (error) {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,8 @@ const auth = require('../middleware/auth');
 router.get('/search', auth, userCtrl.searchUser);
 
 router.get('/user/:id', auth, userCtrl.getUser);
-// router.patch('/user', auth, userCtrl.updateUser);
+
+router.patch('/user', auth, userCtrl.updateUser);
 
 // router.patch('/user/:id/follow', auth, userCtrl.follow);
 // router.patch('/user/:id/unfollow', auth, userCtrl.unfollow);
