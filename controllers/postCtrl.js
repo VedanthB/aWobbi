3bee59e7dbf0c5014a97b3e4318c6703a46020b2
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-19 00:34:43 +0530

    [FEAT] : added get Posts with pagination

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -4,6 +4,24 @@
 const Posts = require('../models/postModel');
 // const User = require('../models/userModel');
 
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  pagination() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 9;
+
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
 const postCtrl = {
   createPost: async (req, res) => {
     try {
@@ -26,6 +44,33 @@ const postCtrl = {
           ...newPost._doc,
           user: req.user,
         },
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  getPosts: async (req, res) => {
+    try {
+      const features = new APIfeatures(
+        Posts.find({ user: [...req.user.following, req.user._id] }), // only find posts of the users the user is following
+        req.query
+      ).pagination();
+
+      const posts = await features.query
+        .sort('-createdAt')
+        .populate('user likes', 'avatar username fullname followers');
+      // .populate({
+      //   path: 'comments',
+      //   populate: {
+      //     path: 'user likes',
+      //     select: '-password',
+      //   },
+      // });
+
+      return res.json({
+        msg: 'Get Post Success!',
+        postLengths: posts.length,
+        posts,
       });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const postCtrl = require('../controllers/postCtrl');
 const auth = require('../middleware/auth');
 
-router.route('/posts').post(auth, postCtrl.createPost);
+router
+  .route('/posts')
+  .post(auth, postCtrl.createPost)
+  .get(auth, postCtrl.getPosts);
 
 module.exports = router;
