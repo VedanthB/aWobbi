7a07327a8415ef5d8cde0d23148f352ed48ea469
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-19 11:18:59 +0530

    [FEAT]: added card footer

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardBody from './Home/PostCard/CardBody';
+import CardFooter from './Home/PostCard/CardFooter';
 import CardHeader from './Home/PostCard/CardHeader';
 
 const PostCard = ({ post }) => {
@@ -7,9 +8,9 @@ const PostCard = ({ post }) => {
     <div className="shadow-lg my-6 border border-gray-200 ">
       <CardHeader post={post} />
       <CardBody post={post} />
-      {/* <CardFooter post={post} />
+      <CardFooter post={post} />
 
-      <Comments post={post} />
+      {/* <Comments post={post} />
       <InputComment post={post} /> */}
     </div>
   );
