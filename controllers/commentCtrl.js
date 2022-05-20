28794315137bf1e0e132effcb66b46aa0bf92146
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-20 08:54:48 +0530

    [FEAT]: added like comment api

--- controllers/commentCtrl.js
+++ controllers/commentCtrl.js
@@ -43,8 +43,46 @@ const commentCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
-  //   updateComment: async (req, res) => {},
-  //   likeComment: async (req, res) => {},
+  updateComment: async (req, res) => {
+    try {
+      const { content } = req.body;
+
+      await Comments.findOneAndUpdate(
+        {
+          _id: req.params.id,
+          user: req.user._id,
+        },
+        { content }
+      );
+
+      res.json({ msg: 'Update Success!' });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      const comment = await Comments.find({
+        _id: req.params.id,
+        likes: req.user._id,
+      });
+
+      if (comment.length > 0)
+        return res.status(400).json({ msg: 'You liked this comment.' });
+
+      await Comments.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { likes: req.user._id },
+        },
+        { new: true }
+      );
+
+      res.json({ msg: 'Liked Comment!' });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
   //   inLikeComment: async (req, res) => {},
   //   deleteComment: async (req, res) => {},
 };
--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -4,9 +4,9 @@ const auth = require('../middleware/auth');
 
 router.post('/comment', auth, commentCtrl.createComment);
 
-// router.patch('/comment/:id', auth, commentCtrl.updateComment);
+router.patch('/comment/:id', auth, commentCtrl.updateComment);
 
-// router.patch('/comment/:id/like', auth, commentCtrl.likeComment);
+router.patch('/comment/:id/like', auth, commentCtrl.likeComment);
 
 // router.patch('/comment/:id/unlike', auth, commentCtrl.unLikeComment);
 
