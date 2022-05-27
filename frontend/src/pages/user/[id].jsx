69ea35cd337a66a6acfded01745661d398ee1507
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-27 08:41:28 +0530

    [STYLES]: profile page tabs

--- frontend/src/pages/user/[id].jsx
+++ frontend/src/pages/user/[id].jsx
@@ -9,7 +9,7 @@ import { useToast } from '../../hooks';
 const Profile = () => {
   const { profile, auth } = useSelector((state) => state);
 
-  const [saveTab, setSaveTab] = useState(false);
+  const [tab, setTab] = useState('Posts');
 
   const dispatch = useDispatch();
   const { showToast } = useToast();
@@ -28,19 +28,35 @@ const Profile = () => {
       <UserInfo id={id} auth={auth} profile={profile} />
 
       {auth.user?._id === id && (
-        <div className="profile_tab">
-          <button
-            className={saveTab ? '' : 'active'}
-            onClick={() => setSaveTab(false)}
-          >
-            Posts
-          </button>
-          <button
-            className={saveTab ? 'active' : ''}
-            onClick={() => setSaveTab(true)}
-          >
-            Saved
-          </button>
+        <div className="w-full flex justify-center">
+          <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+            <ul className="flex flex-wrap -mb-px">
+              <li className="mr-2">
+                <div
+                  onClick={() => setTab('Posts')}
+                  className={`inline-block p-4 rounded-t-lg border-b-2 ${
+                    tab === 'Posts'
+                      ? 'text-purple-600 border-purple-600  dark:text-purple-500 dark:border-purple-500'
+                      : 'border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                  }  `}
+                >
+                  Posts
+                </div>
+              </li>
+              <li className="mr-2">
+                <div
+                  onClick={() => setTab('Saved')}
+                  className={`inline-block p-4 rounded-t-lg border-b-2 ${
+                    tab === 'Saved'
+                      ? 'text-purple-600 border-purple-600  dark:text-purple-500 dark:border-purple-500'
+                      : 'border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                  }  `}
+                >
+                  Saved
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
 
@@ -48,9 +64,8 @@ const Profile = () => {
         <span className="loader block mx-auto"></span>
       ) : (
         <>
-          {saveTab ? (
-            <Saved auth={auth} dispatch={dispatch} />
-          ) : (
+          {tab === 'Saved' && <Saved auth={auth} dispatch={dispatch} />}
+          {tab === 'Posts' && (
             <UserPosts
               auth={auth}
               profile={profile}
