f6d17e4732cd6fede7ec1f9f674e77286050e7ba
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-27 19:04:17 +0530

    [FEAT]: 404 page

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useTheme } from '../context';
 
 const Notfound = () => {
-  return <div>Notfound</div>;
+  const { theme } = useTheme();
+  return (
+    <div className="flex max-w-5xl items-center justify-center mx-auto">
+      <img
+        src={
+          theme === 'light'
+            ? 'https://res.cloudinary.com/supertramp69420/image/upload/v1653658149/404_Error_cgbaqt.gif'
+            : 'https://res.cloudinary.com/supertramp69420/image/upload/v1653658410/404_Error_1_e1afma.gif'
+        }
+        alt="404 "
+      />
+    </div>
+  );
 };
 
 export default Notfound;
