8d6608432a11bb5c9e23f2890137cd97f8acd6ba
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-05-28 15:31:09 +0530

    [FIX]: heroku build fix

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/api', require('./routes/notifyRouter'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
