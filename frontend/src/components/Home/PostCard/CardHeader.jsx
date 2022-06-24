2afa038e95f476c8f2904a9c96ae6c9d00c9c6f0
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-06-24 07:40:20 +0530

    [FIX]:  edit post / delete fix

--- frontend/src/components/Home/PostCard/CardHeader.jsx
+++ frontend/src/components/Home/PostCard/CardHeader.jsx
@@ -40,71 +40,73 @@ const CardHeader = ({ post }) => {
         </div>
       </div>
 
-      <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center items-center rounded-md px-4 py-2  text-sm font-medium text-black dark:text-white focus:outline-purple-400 focus:outline focus:outline-offset-2">
-            <BiDotsVerticalRounded className="text-3xl" />
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50 dark:bg-slate-600">
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    onClick={() =>
-                      dispatch(
-                        setEditPostModal({
-                          editPost: { ...post },
-                          onEdit: true,
-                          isModalOpen: true,
-                        })
-                      )
-                    }
-                    className={`${
-                      active
-                        ? 'bg-violet-500 text-white'
-                        : 'text-gray-900 dark:text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Edit Post
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    onClick={() =>
-                      dispatch(
-                        deletePost({
-                          post,
-                          auth,
-                          showToast,
-                        })
-                      )
-                    }
-                    className={`${
-                      active
-                        ? 'bg-violet-500 text-white'
-                        : 'text-gray-900 dark:text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Delete Post
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
+      {auth.user.userName === post.user.userName && (
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center items-center rounded-md px-4 py-2  text-sm font-medium text-black dark:text-white focus:outline-purple-400 focus:outline focus:outline-offset-2">
+              <BiDotsVerticalRounded className="text-3xl" />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50 dark:bg-slate-600">
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() =>
+                        dispatch(
+                          setEditPostModal({
+                            editPost: { ...post },
+                            onEdit: true,
+                            isModalOpen: true,
+                          })
+                        )
+                      }
+                      className={`${
+                        active
+                          ? 'bg-violet-500 text-white'
+                          : 'text-gray-900 dark:text-white'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Edit Post
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() =>
+                        dispatch(
+                          deletePost({
+                            post,
+                            auth,
+                            showToast,
+                          })
+                        )
+                      }
+                      className={`${
+                        active
+                          ? 'bg-violet-500 text-white'
+                          : 'text-gray-900 dark:text-white'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Delete Post
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      )}
     </div>
   );
 };
