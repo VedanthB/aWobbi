773d60155e10fe54fea4bb5ee85745c6d01046a4
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-06-24 13:57:35 +0530

    [STYLES]: carsouel color dark mode fix

--- frontend/src/components/CreatePostModal.jsx
+++ frontend/src/components/CreatePostModal.jsx
@@ -141,7 +141,7 @@ const CreatePostModal = () => {
               <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all z-50 dark:bg-slate-700 dark:text-white">
                 <Dialog.Title
                   as="h3"
-                  className="text-lg flex justify-between items-center font-medium leading-6 text-gray-900"
+                  className="text-lg flex justify-between items-center font-medium leading-6 text-gray-900 dark:text-white"
                 >
                   Create Post
                   <button
@@ -253,10 +253,6 @@ const CreatePostModal = () => {
                             onClick={handleStream}
                             style={{ width: '2.5rem', height: '2.5rem' }}
                           ></lord-icon>
-                          {/* <AiFillCamera
-                            className="cursor-pointer text-3xl"
-                            onClick={handleStream}
-                          /> */}
 
                           <div className="overflow-hidden my-0 mx-4 relative">
                             <BsImages
